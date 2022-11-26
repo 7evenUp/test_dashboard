6f7a44549773422d600d9b4ad33118caec7e909a
Artyom <sheludeshev.artyom@mail.ru>
2022-11-26 19:31:53 +0300

    working with react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import { BrowserRouter } from 'react-router-dom'
-import Router from './Router'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './pages/ErrorPage'
+import Projects from './pages/Projects'
+import Tasks from './pages/Tasks'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Projects />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/:projectId',
+    element: <Tasks />
+  }
+])
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <div className="min-h-screen flex items-center justify-center">
-        <Router />
-      </div>
-    </BrowserRouter>
+    <div className="min-h-screen flex items-center justify-center">
+      <RouterProvider router={router} />
+    </div>
   )
 }
 
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div>
+      <h1>Выберите проект:</h1>
+      <div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
