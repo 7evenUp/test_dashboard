fbf01d0a2014a15e148d039694b933fe7499ee6b
Artyom <sheludeshev.artyom@mail.ru>
2022-11-28 17:40:54 +0300

    Some styling

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Projects = () => {
   return (
-    <div>
-      <h1>Выберите проект:</h1>
-      <div>
-        <div></div>
+    <div className="flex flex-col gap-14 items-center">
+      <h1 className="text-5xl">Выберите проект:</h1>
+      <div className="flex gap-8">
+        <Link className="border border-slate-400 h-64 p-8 rounded-lg" to={'/1'}>Go to project num: 1</Link>
+        <Link className="border border-slate-400 h-64 p-8 rounded-lg" to={'/2'}>Go to project num: 2</Link>
+        <Link className="border border-slate-400 h-64 p-8 rounded-lg" to={'/3'}>Go to project num: 3</Link>
       </div>
     </div>
   )
